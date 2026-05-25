54ad83e11ce614a5475f558d5909514531c94859
Andrew Dibov <andrew.dibov@ya.ru>
2026-05-26 00:17:28 +0900

    pushka

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ```bash
 sudo chmod +x bash/* && ./bash/init.sh
+source .env # если планируешь работать дальше
 ```
--- bash/init.sh
+++ bash/init.sh
@@ -1,56 +1,46 @@
 printf "Lockbox TF secret name [default : ls--terraform-key] : "
-
 read TF_SECRET_NAME
 TF_SECRET_NAME=${TF_KEY_SECRET_NAME:-"ls--terraform-key"}
 
 printf "Lockbox TF secret key [default : ls__terraform_key] : "
-
 read TF_SECRET_KEY
 TF_SECRET_KEY=${TF_SECRET_KEY:-"ls__terraform_key"}
 
 printf "Lockbox S3 secret name [default : ls--terraform-state] : "
-
 read S3_SECRET_NAME
 S3_SECRET_NAME=${S3_SECRET_NAME:-"ls--terraform-state"}
 
 printf "Lockbox S3 access key entry [default : sa__terraform_static_access_key] : "
-
 read S3_ACCESS_KEY_ENTRY
 S3_ACCESS_KEY_ENTRY=${S3_ACCESS_KEY_ENTRY:-"sa__terraform_static_access_key"}
 
 printf "Lockbox S3 secret key entry [default : sa__terraform_static_secret_key] : "
-
 read S3_SECRET_KEY_ENTRY
 S3_SECRET_KEY_ENTRY=${S3_SECRET_KEY_ENTRY:-"sa__terraform_static_secret_key"}
 
 YC_CLI_INITIALIZATION_SILENCE=true yc storage bucket list
 S3_BUCKET_NAME=""
-
 while [[ -z "$S3_BUCKET_NAME" ]]; do
   printf "S3 terraform state bucket name : "
   read S3_BUCKET_NAME
-
   if [[ -z "$S3_BUCKET_NAME" ]]; then
     YC_CLI_INITIALIZATION_SILENCE=true yc storage bucket list
   fi
 done
 
-# ---
-
 echo "Initializing tfstate to : $S3_BUCKET_NAME"
+
+AWS_ACCESS_KEY_ID=$(YC_CLI_INITIALIZATION_SILENCE=true yc lockbox payload get --name $S3_SECRET_NAME --format json | jq -r --arg key "$S3_ACCESS_KEY_ENTRY" '.entries[] | select(.key == $key) | .text_value')
+AWS_SECRET_ACCESS_KEY=$(YC_CLI_INITIALIZATION_SILENCE=true yc lockbox payload get --name $S3_SECRET_NAME --format json | jq -r --arg key "$S3_SECRET_KEY_ENTRY" '.entries[] | select(.key == $key) | .text_value')
 
 cat > .env <<EOF
 export YC_SERVICE_ACCOUNT_KEY_FILE=terraform.key.json
 export YC_CLOUD_ID=$(yc config get cloud-id)
 export YC_FOLDER_ID=$(yc config get folder-id)
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 EOF
-
-AWS_ACCESS_KEY_ID=$(YC_CLI_INITIALIZATION_SILENCE=true yc lockbox payload get --name $S3_SECRET_NAME --format json | jq -r --arg key "$S3_ACCESS_KEY_ENTRY" '.entries[] | select(.key == $key) | .text_value')
-AWS_SECRET_ACCESS_KEY=$(YC_CLI_INITIALIZATION_SILENCE=true yc lockbox payload get --name $S3_SECRET_NAME --format json | jq -r --arg key "$S3_SECRET_KEY_ENTRY" '.entries[] | select(.key == $key) | .text_value')
-
-echo "export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> .env
-echo "export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> .env
+source .env
 
 YC_CLI_INITIALIZATION_SILENCE=true yc lockbox payload get --name "$TF_SECRET_NAME" --key "$TF_SECRET_KEY" > terraform.key.json
-
 terraform init -backend-config=bucket="$S3_BUCKET_NAME" && terraform apply -auto-approve
