ef427208a6eaaac5e03906c78b3a86c1cb34b328
Andrew Dibov <andrew.dibov@ya.ru>
2026-05-26 11:03:37 +0900

    no use input

--- bash/init.sh
+++ bash/init.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-printf "Lockbox TF secret name [default : ls--terraform-key] : "
-read TF_SECRET_NAME
-TF_SECRET_NAME=${TF_SECRET_NAME:-"ls--terraform-key"}
-
-printf "Lockbox TF secret key [default : ls__terraform_key] : "
-read TF_SECRET_KEY
-TF_SECRET_KEY=${TF_SECRET_KEY:-"ls__terraform_key"}
-
-printf "Lockbox S3 secret name [default : ls--terraform-state] : "
-read S3_SECRET_NAME
-S3_SECRET_NAME=${S3_SECRET_NAME:-"ls--terraform-state"}
-
-printf "Lockbox S3 access key entry [default : sa__terraform_static_access_key] : "
-read S3_ACCESS_KEY_ENTRY
-S3_ACCESS_KEY_ENTRY=${S3_ACCESS_KEY_ENTRY:-"sa__terraform_static_access_key"}
-
-printf "Lockbox S3 secret key entry [default : sa__terraform_static_secret_key] : "
-read S3_SECRET_KEY_ENTRY
-S3_SECRET_KEY_ENTRY=${S3_SECRET_KEY_ENTRY:-"sa__terraform_static_secret_key"}
-
+TF_SECRET_NAME="ls--terraform-key"
+TF_SECRET_KEY="ls__terraform_key"
+S3_SECRET_NAME="ls--terraform-state"
+S3_ACCESS_KEY_ENTRY="sa__terraform_static_access_key"
+S3_SECRET_KEY_ENTRY="sa__terraform_static_secret_key"
 S3_BUCKET_NAME=$(YC_CLI_INITIALIZATION_SILENCE=true yc storage bucket list --format json | jq -r '.[] | select(.name | startswith("sb--terraform-state-")) | .name')
+
 echo "Initializing tfstate to : $S3_BUCKET_NAME"
 
 AWS_ACCESS_KEY_ID=$(YC_CLI_INITIALIZATION_SILENCE=true yc lockbox payload get --name $S3_SECRET_NAME --format json | jq -r --arg key "$S3_ACCESS_KEY_ENTRY" '.entries[] | select(.key == $key) | .text_value')
